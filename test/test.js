6b0ce7fb6bc61e6e332d0643af042a3f160b6aa7
Dmitry Chleck <dmitrychleck@gmail.com>
2012-12-03 18:08:12 +0400

    Initial commit.

---
+++ test/test.js
@@ -0,0 +1,46 @@
+var locale = require('..');
+
+var w = console.log;
+
+var __ = locale.__;
+
+locale.init('i18n');
+locale.to('ru');
+
+w(__());
+
+w(__('1'));
+
+var some_space = 123;
+
+w(__("Hello!# This is comment"));
+
+w(__("Hello!#testid This is comment with id"))
+
+w(__("Hello!#id_without_comment"))
+
+w(__("This is## not comment."))
+
+//__("Bye-bye!")
+
+some_space += 321; // __('Ignored')
+
+var more;
+
+/* Code below will be ignored
+
+__('Ignored again');
+
+*/
+
+w(__('Goodbye...'));
+
+w(__('In phrase \' or ) or __(')); var a = ');'
+
+var wow = '__('; var s = 'Bad string )';
+
+w(__(['%n byte', '%n bytes'], 4));
+
+w(__(['%n error', '%n errors'], 4));
+
+w(__("Long text.\nMultiple lines.\nMore and more."))
